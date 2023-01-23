4c4fbacf672ca91d7bc9ff3a59f57b50fb3570a8
sammmhodge <sam@hodgetts.com>
2023-01-23 22:52:23 +0000

    Done jets and masking
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    I put it down from the whip to my diamonds, I'm in
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    Stay high like how I'm supposed to do
    That crowd underneath them clouds can't get close to you
    And my car look unapproachable
    Super clean, but it's super mean
    She wanna fuck with them cats
    Smoke weed, count stacks, get fly
    Take trips and that's that, real rap
    I let her get high if she want and she feel that
    Convertible drop feel, '87, the top peeled back
    Yeah, uh-huh, you know what it is
    Yeah, yeah, uh-huh, you know what it is
    Y'all already know what it is, man
    If you don't, you should by now
    Reppin' my town, when you see me you know everything
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    I put it down from the whip to my diamonds, I'm in
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    Uh, yeah, uh-huh, you know what it is
    Everything I do, I do it big
    Yeah, uh-huh, screamin', "That's nothin'"
    When I pulled off the lot, that's stuntin'
    Reppin' my town, when you see me you know everything
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    I put it down from the whip to my diamonds, I'm in
    Black and yellow
    Black and yellow
    Black and yellow
    Black and yellow
    
    i AM A MELT

--- AliejnPowerWash/Assets/scripts/GunBehaviour.cs
+++ AliejnPowerWash/Assets/scripts/GunBehaviour.cs
@@ -1,43 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GunBehaviour : MonoBehaviour
 {
     public bool Clicked, rightClicked, leftCloser;
     public GameObject jet, testBall, jet2;
+    private Color[] _colours;
+    private Vector2 lastTouchPos;
+    private bool touchedLastFrame;
     // Start is called before the first frame update
     void Start()
     {
+        
         jet.SetActive(false);
+        jet2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //variable declaration
         Vector3 mousePos = Input.mousePosition;
         Ray castPoint = Camera.main.ScreenPointToRay(mousePos);
         RaycastHit hit;
         Clicked = false;
         rightClicked = false;
+
+        //checks if lmb down
         if (Input.GetMouseButton(0))
         {
+            //fires raycast
             if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
             {
+                //checks for hitting an object with a rigidbody
                 if (hit.rigidbody)
                 {
-                    Vector3 distanceRight = hit.point - jet.transform.position;
-                    Debug.Log("Right " + distanceRight);
-                    Vector3 distanceLeft = hit.point - jet2.transform.position;
-                    Debug.Log("Left " + distanceLeft);
+                    //Saves location that was hit in a variable
                     Vector3 hitSpot = hit.point;
-                    testBall.transform.position = hit.point;
-                    if (distanceLeft.x >= distanceRight.x)
+
+                    //if at or halfway across the screen it will use the right jet otherwise it uses the left one
+                    if (Input.mousePosition.x >= 960)
                     {
+                        //Tells the cone to look at where was hit. This then rotates 180 to get it to face the right way
                         jet.transform.LookAt(hitSpot);
                         jet.transform.rotation *= Quaternion.Euler(0, 180, 0);
 
-
+                        //selects the correct jet
+                        jet.SetActive(true);
+                        jet2.SetActive(false);
+                        
+                        //calculates the distance between the 2 points, this is then used to apply as a scale for the size of the jet to hit the target and maintain a good and consistent size
+                        Vector3 distanceRight = hit.point - jet.transform.position;
                         jet.transform.localScale = new Vector3(distanceRight.z / 2, distanceRight.z / 2, distanceRight.z);
 
                     }
@@ -46,21 +61,22 @@ public class GunBehaviour : MonoBehaviour
                         jet2.transform.LookAt(hitSpot);
                         jet2.transform.rotation *= Quaternion.Euler(0, 180, 0);
 
-
+                        jet2.SetActive(true);
+                        jet.SetActive(false);
+                        Vector3 distanceLeft = hit.point - jet2.transform.position;
                         jet2.transform.localScale = new Vector3(distanceLeft.z / 2, distanceLeft.z / 2, distanceLeft.z);
 
                     }
 
+                    Draw(hit);
+                
                 }
-                    
-                    //Debug.Log(hit.point);
-                    
-                    
-                    //Debug.Log(hit.rigidbody.tag);
+         
             }
             Debug.Log("Left Mouse  pressed 0");
             Clicked = true;
         }
+        else touchedLastFrame = false;
 
         if (Input.GetMouseButton(1))
         {
@@ -68,15 +84,74 @@ public class GunBehaviour : MonoBehaviour
             rightClicked = true;
         }
 
-        if (Clicked == true)
-        {
-            jet.SetActive(true);
-        }
-        else
+        if (Clicked != true)
         {
             jet.SetActive(false);
+            jet2.SetActive(false);
         }
         
 
     }
+
+
+
+
+
+
+
+    private void Draw(RaycastHit hit)
+    {
+        //obtaining variables needed for painting
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCol = hit.collider as MeshCollider;
+        //checks they exist
+        if (rend == null || meshCol == null)
+        {
+            Debug.Log("retrningin");
+            return;
+        }
+
+        //get the location to be painted
+
+        Texture2D tex = rend.material.mainTexture as Texture2D;
+        Vector2 pixelUV = hit.textureCoord;
+        Debug.Log(tex);
+        pixelUV.x *= tex.width;
+        //Debug.Log(pixelUV.x);
+        pixelUV.y *= tex.height;
+        Color _colour = new Vector4(0, 0, 0, 0);
+        _colours = Enumerable.Repeat(Color.clear, 5 * 5).ToArray();
+        var x = (int)(pixelUV.x - (5 / 2));
+        var y = (int)(pixelUV.y - (5 / 2));
+        if (touchedLastFrame)
+        {
+            for (float f = 0.01f; f < 1.00f; f += 0.01f)
+            {
+                //Lerps the painting so that it makes a smooth paint 
+                float distx = lastTouchPos.x - x;
+                //Debug.Log(distx);
+                float disty = lastTouchPos.y - y;
+                Debug.Log(disty);
+                if(distx >= -100f && distx <= 100f && disty >= -100f && disty <= 100f)
+                {
+                    var lerpx = (int)Mathf.Lerp(lastTouchPos.x, x, f);
+                    var lerpy = (int)Mathf.Lerp(lastTouchPos.y, y, f);
+
+                    tex.SetPixels(lerpx, lerpy, 5, 5, _colours);
+                }
+                
+                
+                
+                //tex.SetPixels((int)pixelUV.x, (int)pixelUV.y, 5, 5, _colours);
+            }
+            //paints
+            tex.Apply();
+        }
+        lastTouchPos = new Vector2(x, y);
+        touchedLastFrame = true;
+        return;
+    }
 }
+
+
+
