a0020c2488e2b23d812e528c079e4a748ed47ced
sammmhodge <sam@hodgetts.com>
2023-01-24 00:47:25 +0000

    Revert "Merge branch 'main' of https://github.com/sammmhodge/AlienPowerWash"
    This reverts commit 4e2ce646fbac4d04e1e6b052504bcb659dd6ab0f, reversing
    changes made to 01554769882c6629eb138231ecbc821982638a35.

--- AliejnPowerWash/Assets/scripts/AlienBehaviour.cs
+++ AliejnPowerWash/Assets/scripts/AlienBehaviour.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AlienBehaviour : MonoBehaviour
 {
+    public float rageTotal, rageRemaining, rageLossPerSecond;
+    public Image rageBar, dead;
+   
+    private float timePassed, timeGoal= 1.0f;
     // Start is called before the first frame update
     void Start()
     {
+        dead.color = new Vector4(0, 0, 0, 0);
+        rageRemaining = rageTotal;
         Renderer rend = transform.GetComponent<Renderer>();
         Texture2D tempText = Instantiate(rend.material.mainTexture as Texture2D);
         rend.material.mainTexture = tempText;
@@ -15,6 +22,18 @@ public class AlienBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timePassed >= timeGoal)
+        {
+            rageRemaining -= rageLossPerSecond;
+            timePassed = 0.0f;
+        }
+        else timePassed += Time.deltaTime;
+
+        if(rageRemaining <= 0)
+        {
+            dead.color = new Vector4(1, 1, 1, 1);
+            
+        }
+        rageBar.rectTransform.sizeDelta = new Vector2(1000 * (rageRemaining / rageTotal), 100);
     }
 }
